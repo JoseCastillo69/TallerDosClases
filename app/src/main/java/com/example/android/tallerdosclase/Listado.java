9bdee2ec440efa5be10ad355be75c8e155d33d9f
Jose <josedczarate@hotmail.com>
2018-09-13 19:12:28 -0500

    Interfaz Principal

---
+++ app/src/main/java/com/example/android/tallerdosclase/Listado.java
@@ -0,0 +1,13 @@
+package com.example.android.tallerdosclase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Listado extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado);
+    }
+}
